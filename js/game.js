9230d1a7e0ea7c0eb65fdae0d6cde73a23a44dd0
willstreet <william.alexander13@stjohns.edu>
2017-05-11 12:26:34 -0400

    migrate game data

--- js/game.js
+++ js/game.js
@@ -1,5 +1,30 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameDiv');
 
+var timeNow;
+var bullet;
+var player1;
+var player2;
+var platforms;
+var atms;
+var cursors;
+var Akey;
+var Wkey;
+var Skey;
+var Dkey;
+var balance = 0;
+var newBalance = 0;
+var balanceText;
+var prevShot = 0;
+var Rkey;
+var Mkey;
+var p1Count = 0;
+var p2Count = 0;
+
+//Timer
+var timer;
+var timeLeft = 32;
+var timerText = 0;
+
 game.state.add('boot', bootState);
 game.state.add('load', loadState);
 game.state.add('menu', menuState);
