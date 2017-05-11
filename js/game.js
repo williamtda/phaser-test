98ef1e13e2c29f02177278fed96b08159315e4b6
willstreet <william.alexander13@stjohns.edu>
2017-05-11 12:21:11 -0400

    playState

--- js/game.js
+++ js/game.js
@@ -4,6 +4,6 @@ game.state.add('boot', bootState);
 game.state.add('load', loadState);
 game.state.add('menu', menuState);
 game.state.add('play', playState);
-game.state.add('win', winState);
+//game.state.add('win', winState);
 
 game.state.start('boot');
--- js/play.js
+++ js/play.js
@@ -1,3 +1,17 @@
+var playState = { 
 create : function() {
 	
+	this.keyboard = game.input.keyboard;
+	
+	this.player = game.add.sprite(32, game.world.height - 150, 'businessmoney');
+	
+	game.physics.enable(this.player, Phaser.Physics.ARCADE);
+	
+
+	
+},
+
+update : function() {
+//TODO	
+},
 };
